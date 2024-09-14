921564c89b82063917c30c9b2510fce3a0c1796d
monilprajapati <monillprajapatii@gmail.com>
2024-09-14 16:29:45 +0530

    Added some stuff like navbar and fixed the home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,223 +1,116 @@
 import React, { useState } from "react";
-import { HiOutlineMenuAlt2 } from "react-icons/hi";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-// import { IoFitnessSharp } from "react-icons/io5";
 import { useUserContext } from "../contexts/userContext";
-import UserDropdown from "./UserDropdown";
-
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { logoutUser } from "../services/authServices";
+import { Link } from "react-router-dom";
+import { FaHouseChimneyMedical } from "react-icons/fa6";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog, DialogPanel } from "@headlessui/react";
 
 const Navbar = () => {
-  // this state will tell if user is authenticated or not
-  const navigate = useNavigate();
-  const { setIsAuth, user, isAuth } = useUserContext();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { user } = useUserContext();
 
-
-  const logoutHandler = async () => {
-    try {
-      const response = await logoutUser();
-      if (response.success) {
-        toast.success("Logged out successfully", {
-          duration: 900,
-        });
-        setTimeout(() => {
-          setIsAuth(false); // Update the authentication state
-          navigate("/login"); // Immediately navigate to the login page
-        }, 900);
-      }
-    } catch (error) {
-      console.error("Error logging out:", error);
-      toast.error("Error logging out. Please try again", {
-        duration: 900,
-      });
-    }
-  };
-
+  const navigation = [
+    { name: "Contact Us", href: "/contactus" },
+    { name: "Resources", href: "/resources" },
+  ];
   return (
-    // <>
-    //   <nav className="navbar font-lato font-semibold">
-    //     <Link className="flex items-center gap-2" to="/">
-    //       {/* <span>
-    //         <IoFitnessSharp className="text-xl  md:text-2xl lg:text-4xl xl:text-5xl" />
-    //       </span> */}
-    //       <span className="text-xl  md:text-2xl lg:text-3xl xl:text-4xl">
-    //         Auth
-    //       </span>
-    //     </Link>
-    //     {isAuth && (
-    //       <div className="flex md:hidden">
-    //         <HiOutlineMenuAlt2
-    //           size={30}
-    //           onClick={() => {
-    //             console.log("menu clicked");
-    //           }}
-    //         />
-    //       </div>
-    //     )}
-    //     <div className="hidden md:flex md:w-2/3 justify-end items-center gap-8">
-    //       {isAuth ? (
-    //         <>
-    //           {/* <Link to="/" className="text-xl">
-    //             Ask a Question
-    //           </Link> */}
-    //           <UserDropdown />
-    //         </>
-    //       ) : (
-    //         <button
-    //           className="btn-dark"
-    //           onClick={() => {
-    //             navigate("/login");
-    //           }}
-    //         >
-    //           {window.location.href === "/login" ? "register" : "login"}
-    //         </button>
-    //       )}
-    //     </div>
-    //   </nav>
-    //   <Outlet />
-    // </>
-
-    <Disclosure as="nav" className="bg-white shadow">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button */}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
-              <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
-            </DisclosureButton>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            {/* <div className="flex flex-shrink-0 items-center">
+    <header className="absolute inset-x-0 top-0 z-50 bg-gray-200">
+      <nav
+        aria-label="Global"
+        className="flex items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <Link to={"/"} className="-m-1.5 p-1.5">
+            <span className="sr-only">PostMediCare</span>
+            <div className="flex items-center gap-2">
+              <FaHouseChimneyMedical className="h-8 w-auto" />
+              <span className="text-xl mt-1 font-medium">PostMediCare</span>
+            </div>
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              to={item.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          {!user && (
+            <Link
+              to={"/login"}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Log in <span aria-hidden="true">&rarr;</span>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
               <img
-                alt="Your Company"
+                alt=""
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 className="h-8 w-auto"
               />
-            </div> */}
-            <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-              {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-              <a
-                href="#"
-                className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
-              >
-                Dashboard
-              </a>
-              <a
-                href="#"
-                className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >
-                Team
-              </a>
-              <a
-                href="#"
-                className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >
-                Projects
-              </a>
-              <a
-                href="#"
-                className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >
-                Calendar
-              </a>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            </a>
             <button
               type="button"
-              className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
             >
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="h-6 w-6" />
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
             </button>
-
-            {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
-              <div>
-                {isAuth ?
-                  <MenuButton className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">Open user menu</span>
-                    <img
-                      alt=""
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      className="h-8 w-8 rounded-full"
-                    />
-                  </MenuButton>
-                  :
-                  <button onClick={() => {
-                    navigate("/login");
-                  }}>
-                    Sign In
-                  </button>
-                }
-              </div>
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-              >
-                <MenuItem>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                    Settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <div onClick={logoutHandler} className="block px-4 cursor-pointer py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                    Sign out
-                  </div>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
           </div>
-        </div>
-      </div>
-
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 pb-4 pt-2">
-          {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-          <DisclosureButton
-            as="a"
-            href="#"
-            className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
-          >
-            Dashboard
-          </DisclosureButton>
-          <DisclosureButton
-            as="a"
-            href="#"
-            className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-          >
-            Team
-          </DisclosureButton>
-          <DisclosureButton
-            as="a"
-            href="#"
-            className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-          >
-            Projects
-          </DisclosureButton>
-          <DisclosureButton
-            as="a"
-            href="#"
-            className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-          >
-            Calendar
-          </DisclosureButton>
-        </div>
-      </DisclosurePanel>
-    </Disclosure>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
   );
 };
 
