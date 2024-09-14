74f885db99127385bb8f98f129fede4342e68ec9
monilprajapati <monillprajapatii@gmail.com>
2024-09-14 17:11:33 +0530

    Prod fixes

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -5,7 +5,7 @@ export const ROLES = {
 
 export const COOKIE_OPTIONS = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === 'production' ? true : false,
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // 'none' for production, 'lax' for local
 };
 
