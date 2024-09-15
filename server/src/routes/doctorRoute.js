466180039d281d0742a58bdb1412118c9b290748
Umang Dalvadi <umangdalvadi8@gmail.com>
2024-09-15 07:06:01 +0530

    add getMyPatient

--- server/src/routes/doctorRoute.js
+++ server/src/routes/doctorRoute.js
@@ -3,11 +3,12 @@ const router = Router();
 import { ROLES } from "../utils/constants.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { upload } from "../middlewares/multerMiddleware.js";
-import { handleAddDoctorDetails, handleGetDoctorDetails, handleUpdateDoctorDetails, handleAddPatientByEmail } from "../controllers/doctorController.js";
+import { handleAddDoctorDetails, handleGetDoctorDetails, handleUpdateDoctorDetails, handleAddPatientByEmail, getMyPatients } from "../controllers/doctorController.js";
 
 router.route("/add-details").post(authMiddleware([ROLES.DOCTOR]), upload.single('profilePicture'), handleAddDoctorDetails);
 router.route("/get-details").get(authMiddleware([ROLES.DOCTOR]), handleGetDoctorDetails);
 router.route("/update-details").patch(authMiddleware([ROLES.DOCTOR]), upload.single('profilePicture'), handleUpdateDoctorDetails);
 router.route("/add-patient-by-email").post(authMiddleware([ROLES.DOCTOR]), handleAddPatientByEmail); // New route
+router.route("/get-my-patients").get(authMiddleware([ROLES.DOCTOR]), getMyPatients);
 
 export default router;
