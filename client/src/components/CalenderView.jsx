83116dc5d0c3656cbe6e62366b66c64e5b1584fe
kenil-kanani <kenilkananigecbvn@gmail.com>
2024-09-15 10:29:28 +0530

    fix: change event to medicine-2

--- client/src/components/CalenderView.jsx
+++ client/src/components/CalenderView.jsx
@@ -183,58 +183,6 @@ export default function CalenderView() {
               </button>
             </div>
             <div className="hidden md:ml-4 md:flex md:items-center">
-              {/* <Menu as="div" className="relative"> */}
-              {/* <MenuButton
-                type="button"
-                className="flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              >
-                Month view
-                <ChevronDownIcon
-                  className="-mr-1 h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </MenuButton> */}
-
-              {/* <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-              >
-                <div className="py-1">
-                  <MenuItem>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                    >
-                      Day view
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                    >
-                      Week view
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                    >
-                      Month view
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                    >
-                      Year view
-                    </a>
-                  </MenuItem>
-                </div>
-              </MenuItems> */}
-              {/* </Menu> */}
               <div className="ml-6 h-6 w-px bg-gray-300" />
               <button
                 onClick={() => setOpen(true)}
